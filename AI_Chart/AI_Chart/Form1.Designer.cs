af7e30efac1233b0817c14c8770acc0334e18185
Yip Tsz Ching <23075378d@connect.polyu.hk>
2026-05-09 19:53:16 +0800

    Source code: Chart program

---
+++ AI_Chart/AI_Chart/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AI_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.myNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.passwordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewRecordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteRecordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onlineVersionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItem_Return = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.myNameToolStripMenuItem,
+            this.passwordToolStripMenuItem,
+            this.viewRecordsToolStripMenuItem,
+            this.deleteRecordsToolStripMenuItem,
+            this.onlineVersionToolStripMenuItem,
+            this.aboutMeToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.MenuItem_Return});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(913, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // myNameToolStripMenuItem
+            // 
+            this.myNameToolStripMenuItem.Enabled = false;
+            this.myNameToolStripMenuItem.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myNameToolStripMenuItem.Name = "myNameToolStripMenuItem";
+            this.myNameToolStripMenuItem.Size = new System.Drawing.Size(111, 26);
+            this.myNameToolStripMenuItem.Text = "Your Name";
+            // 
+            // passwordToolStripMenuItem
+            // 
+            this.passwordToolStripMenuItem.Enabled = false;
+            this.passwordToolStripMenuItem.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordToolStripMenuItem.Name = "passwordToolStripMenuItem";
+            this.passwordToolStripMenuItem.Size = new System.Drawing.Size(130, 26);
+            this.passwordToolStripMenuItem.Text = "Set Password";
+            // 
+            // viewRecordsToolStripMenuItem
+            // 
+            this.viewRecordsToolStripMenuItem.Enabled = false;
+            this.viewRecordsToolStripMenuItem.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewRecordsToolStripMenuItem.Name = "viewRecordsToolStripMenuItem";
+            this.viewRecordsToolStripMenuItem.Size = new System.Drawing.Size(118, 26);
+            this.viewRecordsToolStripMenuItem.Text = "Mood Diary";
+            // 
+            // deleteRecordsToolStripMenuItem
+            // 
+            this.deleteRecordsToolStripMenuItem.Enabled = false;
+            this.deleteRecordsToolStripMenuItem.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteRecordsToolStripMenuItem.Name = "deleteRecordsToolStripMenuItem";
+            this.deleteRecordsToolStripMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.deleteRecordsToolStripMenuItem.Text = "Delete Records";
+            // 
+            // onlineVersionToolStripMenuItem
+            // 
+            this.onlineVersionToolStripMenuItem.Enabled = false;
+            this.onlineVersionToolStripMenuItem.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onlineVersionToolStripMenuItem.Name = "onlineVersionToolStripMenuItem";
+            this.onlineVersionToolStripMenuItem.Size = new System.Drawing.Size(140, 26);
+            this.onlineVersionToolStripMenuItem.Text = "Online Version";
+            // 
+            // aboutMeToolStripMenuItem
+            // 
+            this.aboutMeToolStripMenuItem.Enabled = false;
+            this.aboutMeToolStripMenuItem.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutMeToolStripMenuItem.Name = "aboutMeToolStripMenuItem";
+            this.aboutMeToolStripMenuItem.Size = new System.Drawing.Size(100, 26);
+            this.aboutMeToolStripMenuItem.Text = "About Me";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Enabled = false;
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(53, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // MenuItem_Return
+            // 
+            this.MenuItem_Return.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MenuItem_Return.Name = "MenuItem_Return";
+            this.MenuItem_Return.Size = new System.Drawing.Size(65, 26);
+            this.MenuItem_Return.Text = "Close";
+            this.MenuItem_Return.Click += new System.EventHandler(this.MenuItem_Return_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(7, 37);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(900, 453);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "Chart";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(901, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(913, 499);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lumen Buddy - An Emotional Support AI Chatbot - Your little light of hope, always" +
+    " here to listen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem myNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem passwordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewRecordsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteRecordsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem onlineVersionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutMeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuItem_Return;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ AI_Chart/AI_Chart/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace AI_Chart
+{
+    public partial class Form1 : Form
+    {
+        string username = "whoiam";
+        string password = "";
+        string[] argVal;
+
+        public Form1(string[] argv)
+        {
+            argVal = argv;
+            InitializeComponent();
+        }
+
+        private string encrypt(string s)
+        {
+            int i;
+            Random rand = new Random();
+            string retS = Convert.ToChar(33 + s.Length).ToString();
+            for (i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                int code = 158 - (((c & 3) * 4) | ((c & 12) / 4) | (c & 240));
+                retS += Convert.ToChar(code);
+            }
+            for (i = s.Length; i < 20; i++)
+                retS += Convert.ToChar(rand.Next(32, 127));
+            return (retS);
+        }
+
+        private string decrypt(string s)
+        {
+            int i, n = s[0] - 33;
+            string retS = "";
+            for (i = 1; i <= n; i++)
+            {
+                char c = s[i];
+                int code = 158 - c;
+                code = ((code & 3) * 4) | ((code & 12) / 4) | (code & 240);
+                retS += Convert.ToChar(code);
+            }
+            return (retS);
+        }
+
+        private string encryptRecord(string s)
+        {
+            int i;
+            string retS = "";
+            for (i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                int code = 158 - (((c & 3) * 4) | ((c & 12) / 4) | (c & 240));
+                retS += Convert.ToChar(code);
+            }
+            return (retS);
+        }
+
+        private string decryptRecord(string s)
+        {
+            int i;
+            string retS = "";
+            for (i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                int code = 158 - c;
+                code = ((code & 3) * 4) | ((code & 12) / 4) | (code & 240);
+                retS += Convert.ToChar(code);
+            }
+            return (retS);
+        }
+
+        private void MenuItem_Return_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int argc = argVal.Length;
+            //textBox1.Text = "arg: " + argc;
+            //foreach (string a in argVal)
+            //    textBox1.Text += " | " + a;
+
+            try
+            {
+                StreamReader sr = new StreamReader("system.dat");
+                string s = sr.ReadLine();
+                //if (s.Length > 0) username = decrypt(s);
+                s = sr.ReadLine();
+                string pp = decrypt(s);
+                if (s.Length > 0)
+                {
+                    if (argc < 3)
+                        Application.Exit();
+
+                    //textBox1.Text += " $ " + pp;
+                    //textBox1.Text += " | " + decrypt(argVal[1]);
+
+                    if (!pp.Equals(decrypt(argVal[argc-1])))
+                        Application.Exit();
+                }
+                sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (Exception)
+            {
+            }
+
+            chart1.Width = this.Width - 30;
+            chart1.Height = this.Height - 80;
+            int n = 0;
+            string date, cDate = "01-01-2025";
+            int dayN = 0, dayScore = 0;
+
+            try
+            {
+                StreamReader sr = new StreamReader("records.dat");
+                chart1.Series[0].Points.Clear();
+                while (!sr.EndOfStream)
+                {
+                    string s = decryptRecord(sr.ReadLine());
+                    if (s[0] == '`')
+                    {
+                        if (s[1] == '^')
+                        {
+                            date = s.Substring(2);
+                            if (n > 0 && date != cDate)
+                                chart1.Series[0].Points.AddXY(cDate, Convert.ToDouble(dayScore) / Convert.ToDouble(dayN));
+                            cDate = date;
+                            dayN = 0;
+                            dayScore = 0;
+                        }
+                        if (s[1] == '|')
+                        {
+                            dayScore += Convert.ToInt32(s.Substring(2));
+                            dayN++;
+                            n++;
+                        }
+                    }
+                }
+                sr.Close();
+                double avg = Convert.ToDouble(dayScore) / Convert.ToDouble(dayN);
+                chart1.Series[0].Points.AddXY(cDate, avg);
+                chart1.Visible = true;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+    }
+}
